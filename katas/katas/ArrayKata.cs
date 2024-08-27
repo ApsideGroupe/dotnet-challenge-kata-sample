0dba123ef80061a3b9a8b36f8928b97d13cba88c
Ruiz Alexandre <rruiz.alex@gmail.com>
2024-08-27 11:10:14 +0200

    Method description

--- katas/katas/ArrayKata.cs
+++ katas/katas/ArrayKata.cs
@@ -2,6 +2,9 @@
 
 public static class ArrayKata
 {
+    ///<summary>
+    /// This method merge 2 arrays into a single one, sorted in asc order
+    ///</summary>
     public static int[] MergeArrays(int[] arr1, int[] arr2)
     {
         throw new NotImplementedException();
