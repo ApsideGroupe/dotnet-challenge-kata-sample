42137bbf38aa896cd6438806fa13fd024d949c39
Julien Creach <julien.creach@pm.me>
2024-08-30 09:53:26 +0200

    fix merge trouble (code missing) (#3)
    Co-authored-by: Julien Creach <julien.creach@apside.com>

--- katas/katas.sample.test/ArrayKataTests.cs
+++ katas/katas.sample.test/ArrayKataTests.cs
@@ -3,21 +3,24 @@ using katas.sample;
 [TestFixture]
 public class KataTests
 {
-    [Test]
-    public void SampleTest()
+    [TestCase(new[] { 1, 2, 3, 4 }, new[] { 5, 6, 7, 8 }, new[] { 1, 2, 3, 4, 5, 6, 7, 8 })]
+    [TestCase(new[] { 1, 3, 5, 7, 9 }, new[] { 10, 8, 6, 4, 2 }, new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 })]
+    [TestCase(new[] { 1, 3, 5, 7, 9, 11, 12 }, new[] { 1, 2, 3, 4, 5, 10, 12 }, new[] { 1, 2, 3, 4, 5, 7, 9, 10, 11, 12 })]
+    [TestCase(new int[] { }, new int[] { }, new int[] { })]
+    [TestCase(new[] { 1, 2, 3 }, new int[] { }, new[] { 1, 2, 3 })]
+    [TestCase(new int[] { }, new [] { 1, 2, 3, 4, 5 }, new [] { 1, 2, 3, 4, 5 })]
+    [TestCase(new[] { -3, -2, -1, 0 }, new [] { 1, 2, 3, 4 }, new [] { -3, -2, -1, 0, 1, 2, 3, 4 } )]
+    public void SampleTest(int[] input1, int[] input2, int[] result)
     {
-        Assert.AreEqual(new[] { 1, 2, 3, 4, 5, 6, 7, 8 }, ArrayKata.MergeArrays(new[] { 1, 2, 3, 4 }, new[] { 5, 6, 7, 8 }));
+        // Arrange
 
-        Assert.AreEqual(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, ArrayKata.MergeArrays(new[] { 1, 3, 5, 7, 9 }, new[] { 10, 8, 6, 4, 2 }));
+        // Act
 
-        Assert.AreEqual(new[] { 1, 2, 3, 4, 5, 7, 9, 10, 11, 12 }, ArrayKata.MergeArrays(new[] { 1, 3, 5, 7, 9, 11, 12 }, new[] { 1, 2, 3, 4, 5, 10, 12 }));
+        var mergedArrays = ArrayKata.MergeArrays(input1, input2);
 
-        Assert.AreEqual(new int[] { }, ArrayKata.MergeArrays(new int[] { }, new int[] { }));
+        //Assert
 
-        Assert.AreEqual(new[] { 1, 2, 3 }, ArrayKata.MergeArrays(new[] { 1, 2, 3 }, new int[] { }));
-
-        Assert.AreEqual(new int[] { 1, 2, 3, 4, 5 }, ArrayKata.MergeArrays(new int[] { }, new int[] { 1, 2, 3, 4, 5 }));
-
-        Assert.AreEqual(new int[] { -3, -2, -1, 0, 1, 2, 3, 4 }, ArrayKata.MergeArrays(new[] { -3, -2, -1, 0 }, new int[] { 1, 2, 3, 4 }));
+        Assert.That(mergedArrays.Length, Is.EqualTo(result.Length));
+        Assert.That(mergedArrays, Is.EqualTo(result));
     }
 }
